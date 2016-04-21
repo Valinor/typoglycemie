16a8c483cf49284a6ca0bbcee472b3f1a248cc8f
Valinor <adrien.henriet@gmail.com>
2016-04-21 22:05:07 +0200

    correct test

--- unit/test_typoglycemie.py
+++ unit/test_typoglycemie.py
@@ -1,9 +1,9 @@
 import unittest
-import typoglycemiet
+import typoglycemie
 
 class TestRandomizeMethods(unittest.TestCase):
     def test_shortword_01(self):
-        self.assertEqual(typoglycemiet.typoglycemie("aze"),"aze")
+        self.assertEqual(typoglycemie.typoglycemie("aze"),"aze")
 
 
 if __name__== '__main__':
