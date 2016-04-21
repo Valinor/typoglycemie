1b42d2c81e6a99e2cc79434c521f12a786577c35
Valinor <adrien.henriet@gmail.com>
2016-04-21 21:59:26 +0200

    add unit test

--- typoglycemiet.py
+++ typoglycemiet.py
@@ -1,29 +1,30 @@
 import sys
 import random
 
-def main(data):
+def randomize(word,change_number=50):
+    word_array=list(word[1:-1])
+    size=len(word_array)
+    for _ in range(change_number):
+        first_letter=random.randrange(0,size)
+        second_letter=random.randrange(0,size)
+        mem=word_array[first_letter]
+        word_array[first_letter]=word_array[second_letter]
+        word_array[second_letter]=mem
+    return word[0]+"".join(word_array)+word[-1]
+
+def typoglycemie(data):
     data=data.split(" ")
     result=[]
-    for mot in data:
-        if len(mot)<4:
-            result.append(mot)
+    for word in data:
+        if len(word)<4:
+            result.append(word)
         else:
-            word_array=list(mot[1:-1])
-            size=len(word_array)
-            for _ in range(50):
-                un=random.randrange(0,size-1)
-                deux=random.randrange(0,size-1)
-                mem1=word_array[un]
-                word_array[un]=word_array[deux]
-                word_array[deux]=mem1
-            mot=mot[0]+"".join(word_array)+mot[-1]
-            result.append(mot)
+            result.append(randomize(word))
     return " ".join(result)
 
 if __name__ == '__main__':
-    print "test"+str(len(sys.argv))+sys.argv[0]
     if len(sys.argv)>1:
         with open(sys.argv[1]) as f:
             result=f.readlines()
             for phrase in result:
-                print main(phrase)
+                print typoglycemie(phrase)
